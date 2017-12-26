79f250c11edb3bdc6801f658a4fb19bbd54b3049
dharmendraverma <dharmendra@hike.in>
2017-12-26 16:39:24 +0530

    first commit

---
+++ app/src/main/java/com/example/dharmendraverma/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.dharmendraverma.myapplication;
+
+import android.content.res.Resources;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    protected Resources mResources;
+
+   // private VideoSurfaceView mVideoView = null;
+    String mVideoFilepath = null;
+    GLSurfaceView mGLSurfaceView;
+    private VideoRender mRenderer;
+/*    private MediaPlayer mMediaPlayer = null;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mResources = getResources();
+        /*mMediaPlayer = new MediaPlayer();*/
+        mVideoFilepath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        /*mMediaPlayer = new MediaPlayer();*/
+      /*  try {
+            mMediaPlayer.setDataSource(this,Uri.parse(mVideoFilepath));
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }*/
+       // mMediaPlayer.prepareAsync();
+        mRenderer = new VideoRender(R.raw.video,this);
+        mGLSurfaceView = (GLSurfaceView)findViewById(R.id.glsurfaceview);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView.setRenderer(mRenderer);
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+      //  mVideoView = new VideoSurfaceView(R.raw.video,this);
+        /*setContentView(mVideoView);*/
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+       // mVideoView.onResume();
+    }
+
+}
